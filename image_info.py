c565ab5cdc80f4390d786dee208c3c26d00040c5
Mike Jancar <mikejancar@gmail.com>
2018-12-28 08:24:01 -0500

    Can now determine year taken without good EXIF data

--- image_info.py
+++ image_info.py
@@ -1,10 +1,20 @@
 """Get image metadata"""
 
-from PIL import Image
-
-DATETIME_TAKEN = 36867
+from datetime import datetime
+import os
+import exifread
 
 def get_year_taken(image_path):
   """Get the year an image was taken"""
-  image_taken_at = Image.open(image_path)._getexif()[DATETIME_TAKEN]
-  return image_taken_at.split(':')[0]
+  image_file = open(image_path, 'rb')
+  image_tags = exifread.process_file(image_file)
+
+  if 'EXIF DateTimeOriginal' in image_tags:
+      date_taken = datetime.strptime(image_tags['EXIF DateTimeOriginal'].printable,
+                                     '%Y:%m:%d %H:%M:%S')
+      return date_taken.year
+  else:
+      full_file_name = os.path.basename(image_path)
+      short_file_name = os.path.splitext(full_file_name)[0]
+      date_taken = datetime.strptime(short_file_name, '%Y-%m-%d %H.%M.%S')
+      return str(date_taken.year)
