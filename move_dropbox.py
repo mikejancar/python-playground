8cb32b0416b1dd8a522c98f0bebf77fa6add9195
Mike Jancar <mikejancar@gmail.com>
2021-03-23 19:06:06 -0400

    Updated source and targets for new drive

--- move_dropbox.py
+++ move_dropbox.py
@@ -3,8 +3,8 @@
 import media_mover
 
 DROPBOX_FOLDER = 'C:/Users/Mikej/Dropbox/Camera Uploads'
-PICTURE_FOLDER = 'D:/Pictures'
-VIDEO_FOLDER = 'D:/Video/Home Movies'
+PICTURE_FOLDER = 'C:/Media/Pictures'
+VIDEO_FOLDER = 'C:/Media/Video/Home Movies'
 
 media_mover.move_pictures(DROPBOX_FOLDER, PICTURE_FOLDER)
 media_mover.move_videos(DROPBOX_FOLDER, VIDEO_FOLDER)
